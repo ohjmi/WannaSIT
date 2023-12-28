48fd83f33aca8afb374a787b51fe6f6a0f449eb5
ohjmi <ohjmi@naver.com>
2023-12-28 23:38:05 +0900

    [Feat] 추천 호차 랭킹 사용자 지정 출발지 도착지 정보 구현 추천 랭킹 가상 데이터로 서버 구축 후 요청받아 리스트 구현

--- my-react-app/src/index.js
+++ my-react-app/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import Hamburger from './components/Hamburger/Hamburger';
+// import Hamburger from './components/Hamburger/Hamburger';
+// import BottomNav from './components/BottomNav/BottomNav';
+import StationPath from './components/StationPath/StationPath';
+import CarRank from './components/CarRank/CarRank';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Hamburger />
+    <StationPath />
+    <CarRank />
   </React.StrictMode>
 );
 
