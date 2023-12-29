c83bf9ed8e3871bbf4e293966ea8f4dfaa15d6f4
WhyBusyy <ybg6152@naver.com>
2023-12-30 00:38:36 +0900

    [chore] 백엔드 연동을 위한 수정

--- my-react-app/src/pages/RecommendPage/Recommend.js
+++ my-react-app/src/pages/RecommendPage/Recommend.js
@@ -1,8 +1,12 @@
 import "./Recommend.css";
+import StationPath from "../../components/StationPath/StationPath";
+import CarRank from "../../components/CarRank/CarRank";
 
 function Recommend() {
   return (
     <div className="Recommend">
+      <StationPath />
+      <CarRank />
     </div>
   );
 }
--- server/app.js
+++ server/app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import config from "./src/config/index.js";
 
-import carsRouter from "./src/routes/carsRoutes.js";
+// import carsRouter from "./src/routes/carsRoutes.js";
 import stationsRouter from "./src/routes/stationsRoutes.js";
 
 const app = express();
@@ -11,7 +11,7 @@ const port = config.port;
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/stations", stationsRouter);
-app.use("/cars", carsRouter);
+// app.use("/cars", carsRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
