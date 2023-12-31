b6dc1d69c04cdb8e5f843b0b3ae3ef8cdc9d5a9c
WhyBusyy <ybg6152@naver.com>
2023-12-31 22:59:54 +0900

    [chore] 디테일페이지 - 구조 잡기 끝 - 버튼 기능 완료

--- my-react-app/src/pages/RecommendDetailPage/RecommendDetail.js
+++ my-react-app/src/pages/RecommendDetailPage/RecommendDetail.js
@@ -1,30 +1,67 @@
 import "./RecommendDetail.css";
 import StationPath from "../../components/StationPath/StationPath";
-import { useLocation } from 'react-router-dom';
-import greenCircle from "../../assets/images/icon/greenCircle.svg"
-import yellowCircle from "../../assets/images/icon/yellowCircle.svg"
-import redCircle from "../../assets/images/icon/redCircle.svg"
+import { useLocation } from "react-router-dom";
+import greenCircle from "../../assets/images/icon/greenCircle.svg";
+import yellowCircle from "../../assets/images/icon/yellowCircle.svg";
+import redCircle from "../../assets/images/icon/redCircle.svg";
+import { useState } from "react";
 
 function RecommendDetail() {
   const location = useLocation();
   const query = new URLSearchParams(location.search);
-  // const startStation = query.get('startStation');
-  // const endStation = query.get('endStation');
-  const carNumber = query.get('carNumber');
+  const startStation = query.get('startStation');
+  const endStation = query.get('endStation');
+  const carNumber = query.get("carNumber");
+
+  const [openChance, setOpenChance] = useState(true);
+  const [chanceColor, setChanceColor] = useState("#C0EF52");
+  const [openHighCars, setOpenHighCars] = useState(false);
+  const [highCarsColor, setHighCarsColor] = useState("white");
+
+  const handleChance = () => {
+    setOpenChance(true);
+    setOpenHighCars(false);
+    setChanceColor("#C0EF52")
+    setHighCarsColor("white")
+  };
+
+  const handleHighCars = () => {
+    setOpenChance(false);
+    setOpenHighCars(true);
+    setChanceColor("white")
+    setHighCarsColor("#C0EF52")
+  };
 
   return (
     <div className="RecommendDetail">
-      <StationPath />
+      <StationPath startStation={startStation} endStation={endStation} />
       <div className="carSelectOpt">
-        <button>{carNumber}</button>
-        <button>칸 이동</button>
+        <div className="openChance" onClick={handleChance} style={{backgroundColor : chanceColor}}>
+          {carNumber}
+        </div>
+        <div className="openHighCars" onClick={handleHighCars} style={{backgroundColor : highCarsColor}}>
+          칸 이동
+        </div>
       </div>
-      <div className="carSelected">
-        <p>앉을 가능성을 알려드려요<img src={greenCircle} alt="초록원" /><img src={yellowCircle} alt="노란원" /><img src={redCircle} alt="빨간원" /></p>
-      </div>
-      <div className="allStationInPath"></div>
-      <div className="chanceOfCar"></div>
-      <div></div>
+
+      <div className="StationInPath"></div>
+
+      {openChance && (
+        <div className="Chance">
+          <p>
+            앉을 가능성을 알려드려요
+            <img src={greenCircle} alt="초록원" />
+            <img src={yellowCircle} alt="노란원" />
+            <img src={redCircle} alt="빨간원" />
+          </p>
+        </div>
+      )}
+
+      {openHighCars && (
+        <div className="HighCars">
+          <p>앉을 가능성이 높은 호차를 알려드려요</p>
+        </div>
+      )}
     </div>
   );
 }
