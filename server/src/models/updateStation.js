daca77f5542fcc998f31530b69848c2a9447fafe
rhtkdals135 <rhtkdals135@gmail.com>
2024-01-04 00:28:00 +0900

    [Feat] 착석 꿀팁 JSON 파일 작성 및 station 테이블 update 코드 작성 #16 - 각 역별 특성 및 주요 이용객 stationTips.json에 작성 - stationTips.json 파일을 읽어서 station 테이블에 update하는 코드 updateStation.js에 작성

--- server/src/models/updateStation.js
+++ server/src/models/updateStation.js
@@ -5,6 +5,7 @@ import path from "path";
 
 const updateStation = async () => {
 	const connection = await mysql.createConnection(process.env.DATABASE_URL);
+	// 이후 착석꿀팁 관련 수정사항은 stationTips.json 파일에 작성하고 updateStation 함수만 호출하면 DB UPDATE 가능
 	const stationTipsData = JSON.parse(fs.readFileSync(path.join("../", "data", "stationTips.json")));
 
 	try {
