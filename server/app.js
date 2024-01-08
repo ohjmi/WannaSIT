f807cdcc602494d66b4de98a75974fde24c341ba
WhyBusyy <ybg6152@naver.com>
2024-01-08 18:23:25 +0900

    [Fix] 웹소켓통신 - 서버 연동 후 데이터 바인딩 양식 수정

--- server/app.js
+++ server/app.js
@@ -1,13 +1,30 @@
 import express from "express";
-import config from "./src/config/index.js";
 import cors from "cors";
+import expressWs from "express-ws";
+import session from "express-session";
+
+import config from "./src/config/index.js";
 
 import carsRouter from "./src/routes/carsRoutes.js";
 import stationsRouter from "./src/routes/stationsRoutes.js";
 
+import setupWebSocket from "./webSocket.js";
+// import path from "path";
+// const scriptDirectory = path.resolve();
+// const filePath = path.join(scriptDirectory, "public", "client3.html");
+
 const app = express();
+expressWs(app);
 
 const port = config.port;
+
+app.use(
+  session({
+    secret: config.sessionSecret,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
 
 const corsOptions = {
   origin: ["http://localhost:3000"],
@@ -18,6 +35,18 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/stations", stationsRouter);
 app.use("/cars", carsRouter);
+
+// // ========== 웹 소켓 테스트
+// app.use(express.static("public"));
+// app.get("/", (req, res) => {
+//   res.sendFile(filePath);
+// });
+// // ==========
+
+const sessionInfo = new Map();
+const wsClients = new Map();
+
+setupWebSocket(app, sessionInfo, wsClients);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
