f726664c241f992ec895cbff9d9ec7f093bcb276
rhtkdals135 <rhtkdals135@gmail.com>
2024-01-16 01:08:00 +0900

    [Feat] 게시판 API 라우터, 컨트롤러 작성 #49

---
+++ server/src/routes/boardsRoutes.js
@@ -0,0 +1,11 @@
+import express from "express";
+import boardsController from "../controllers/boardsController.js"
+
+const router = express.Router();
+
+router.get("/");
+router.post("/");
+router.put("/");
+router.delete("/");
+
+export default router;
