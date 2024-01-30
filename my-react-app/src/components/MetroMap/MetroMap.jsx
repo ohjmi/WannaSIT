dbb8bd81cc2f6a7774ac8bbfe185fd91ccea742e
WhyBusyy <ybg6152@naver.com>
2024-01-30 21:03:47 +0900

    [Fix] 노선도 #41 해상도에 따라 마커 위치 오류로 인한 수정
    - 기존 canvas 레이어 방식 >> svg 태그를 컴포넌트로 활용하는 방식으로 변경
    - 줌 기능은 react-zoom-pan-pinch 라이브러리로 대체
    - svg 태그 좌표와 상대 좌표 계산 후 마커 생성
    - 속성 data-value로 역명 부여, 해당 값을 Search컴포넌트로 전달

--- my-react-app/src/components/MetroMap/MetroMap.jsx
+++ my-react-app/src/components/MetroMap/MetroMap.jsx
@@ -1,302 +1,87 @@
-import React, { useRef, useEffect, useState } from "react";
-import "./MetroMap.css";
-import Map from "../../assets/images/MetroMap.svg";
-import marker from "../../assets/images/marker.svg";
+import React, { useState, useRef } from "react";
+import { TransformWrapper, TransformComponent } from "react-zoom-pan-pinch";
+import { ReactComponent as MapSvg } from "../../assets/images/MetroMap.svg";
+import { ReactComponent as MarkerSvg } from "../../assets/images/marker.svg";
 
 function MetroMap({ startResultClick, endResultClick }) {
-  // 노선도, 마커 생성을 위한 변수
-  const mapCanvasRef = useRef(null);
-  const markerCanvasRef = useRef(null);
-  const dpr = window.devicePixelRatio;
-  const [isMarkerTrue, setIsMarkerTrue] = useState(false);
-  const [locationValue, setLocationValue] = useState(false);
+  const [markerDiv, setMarkerDiv] = useState();
+  const svgRef = useRef();
 
-  // 노선도 zoom, panning 기능을 위한 변수
-  const mapRef = useRef();
-  const [transform, setTransform] = useState({ x: 0, y: 0, scale: 0.57 });
-  const [dragging, setDragging] = useState(false);
-  const [lastPosition, setLastPosition] = useState(null);
-  const [scale, setScale] = useState(0.57);
-  const minScale = 0.4;
-  const maxScale = 1.1;
-  const minX = -150;
-  const maxX = 150;
-  const minY = -400;
-  const maxY = 250;
-
-  // canvas를 활용한 노선도 마운트
-  useEffect(() => {
-    const mapCanvas = mapCanvasRef.current;
-    const markerCanvas = markerCanvasRef.current;
-    const mapCtx = mapCanvas.getContext("2d");
-    // 지도 이미지 로드
-    const mapImage = new Image();
-    mapImage.src = Map;
-
-    mapImage.onload = () => {
-      mapCanvas.width = mapImage.width * dpr;
-      mapCanvas.height = mapImage.height * dpr * 1.05;
-      markerCanvas.width = mapImage.width * dpr;
-      markerCanvas.height = mapImage.height * dpr * 1.05;
-      mapCtx.scale(dpr, dpr);
-      mapCtx.drawImage(mapImage, 0, 20, mapImage.width, mapImage.height);
-    };
-  }, []);
-
-  // 마우스 휠 이벤트 핸들링
-  const handleWheel = (event) => {
-    event.preventDefault();
-    const newScale = scale + (event.deltaY < 0 ? 0.05 : -0.05);
-    if (newScale >= minScale && newScale <= maxScale) {
-      setScale(newScale);
-      setTransform((prev) => ({
-        x: prev.x,
-        y: prev.y,
-        scale: newScale,
-      }));
-    }
-  };
-
-  useEffect(() => {
-    const mapElement = mapRef.current;
-    mapElement.addEventListener("wheel", handleWheel);
-    return () => {
-      mapElement.removeEventListener("wheel", handleWheel);
-    };
-  }, [handleWheel]);
-
-  // 클릭 드래그 이벤트 핸들링
-  const handleMouseDown = (event) => {
-    event.preventDefault();
-    setLastPosition({ x: event.clientX, y: event.clientY });
-    setDragging(true);
-  };
-
-  const handleMouseUp = () => {
-    setDragging(false);
-  };
-
-  const handleMouseMove = (event) => {
-    event.preventDefault();
-    if (dragging) {
-      const deltaX = event.clientX - lastPosition.x;
-      const deltaY = event.clientY - lastPosition.y;
-      const newX = transform.x + deltaX;
-      const newY = transform.y + deltaY;
-      if (newX >= minX && newX <= maxX && newY >= minY && newY <= maxY) {
-        setTransform({
-          x: newX,
-          y: newY,
-          scale: transform.scale,
-        });
-      }
-      setLastPosition({ x: event.clientX, y: event.clientY });
-    }
-  };
-
-  const handleMouseLeave = () => {
-    setDragging(false);
-  };
-
-  // 터치 드래그 이벤트 핸들링
-  const handleTouchStart = (event) => {
-    setLastPosition({
-      x: event.touches[0].clientX,
-      y: event.touches[0].clientY,
-    });
-    setDragging(true);
-  };
-
-  const handleTouchEnd = () => {
-    setDragging(false);
-  };
-
-  const handleTouchMove = (event) => {
-    if (dragging) {
-      const deltaX = event.touches[0].clientX - lastPosition.x;
-      const deltaY = event.touches[0].clientY - lastPosition.y;
-      const newX = transform.x + deltaX;
-      const newY = transform.y + deltaY;
-      if (newX >= minX && newX <= maxX && newY >= minY && newY <= maxY) {
-        setTransform({
-          x: newX,
-          y: newY,
-          scale: transform.scale,
-        });
-      }
-      setLastPosition({
-        x: event.touches[0].clientX,
-        y: event.touches[0].clientY,
-      });
-    }
-  };
-
-  // 역 데이터 매핑
-  const stationCoordinates = {
-    을지로입구: [356, 80],
-    시청: [269, 80],
-    충정로: [189, 176],
-    아현: [189, 236],
-    이대: [189, 296],
-    신촌: [189, 357],
-    홍대입구: [189, 417],
-    합정: [189, 478],
-    당산: [189, 538],
-    영등포구청: [189, 599],
-    문래: [189, 661],
-    신도림: [189, 722],
-    대림: [189, 781],
-    구로디지털단지: [189, 844],
-    신대방: [189, 902],
-    신림: [189, 961],
-    봉천: [189, 1024],
-    서울대입구: [189, 1083],
-    낙성대: [189, 1144],
-    사당: [189, 1205],
-    방배: [189, 1265],
-    서초: [260, 1357],
-    교대: [362, 1357],
-    을지로3가: [425, 132],
-    을지로4가: [425, 192],
-    동대문역사문화공원: [425, 252],
-    신당: [425, 312],
-    상왕십리: [425, 369],
-    왕십리: [425, 428],
-    한양대: [425, 487],
-    뚝섬: [425, 547],
-    성수: [425, 607],
-    건대입구: [425, 664],
-    구의: [425, 723],
-    강변: [425, 783],
-    잠실나루: [425, 842],
-    잠실: [425, 902],
-    잠실새내: [425, 961],
-    종합운동장: [425, 1021],
-    삼성: [425, 1080],
-    선릉: [425, 1139],
-    역삼: [425, 1199],
-    강남: [425, 1258],
-  };
-
-  // 클릭 이벤트 핸들러
   const handleMapClick = (event) => {
-    const rect = event.target.getBoundingClientRect();
-    const scaleX = event.target.width / rect.width;
-    const scaleY = event.target.height / rect.height;
+    // 이미 생성된 마커가 있다면 제거
+    setMarkerDiv(null);
 
-    const offsetX = (event.clientX - rect.left) * scaleX;
-    const offsetY = (event.clientY - rect.top) * scaleY;
+    // 클릭 좌표 확인
+    const clickX = event.clientX;
+    const clickY = event.clientY;
 
-    console.log("클릭위치", offsetX, offsetY);
-    const locationValue = getLocationValue(offsetX, offsetY);
+    // 클릭한 좌표에 있는 요소 가져오기
+    const targetPath = document.elementFromPoint(clickX, clickY);
 
-    if (isMarkerTrue) {
-      markerClickEvent(offsetX, offsetY);
-    } else if (locationValue) {
-      setLocationValue(locationValue);
-      drawMarker(
-        locationValue.station,
-        locationValue.stationX - 61,
-        locationValue.stationY - 60
+    if (targetPath.classList.contains("station")) {
+      // targetPath의 value 가져오기
+      const targetStaion = targetPath.getAttribute("data-value");
+      // targetPath의 위치와 크기 정보 가져오기
+      const { left, top, width, height } = targetPath.getBoundingClientRect();
+      // targetPath의 좌표 중앙값 계산
+      const centerX = left + width / 2;
+      const centerY = top + height / 2;
+
+      // 새 SVGPoint 객체를 생성 (SVG 파일의 한 점을 나타냄)
+      const svg = svgRef.current;
+      const point = svg.createSVGPoint();
+      // point의 좌표 = targetPath의 좌표 중앙값
+      point.x = centerX;
+      point.y = centerY;
+
+      const transformedPoint = point.matrixTransform(
+        svg.getScreenCTM().inverse()
       );
+      // getScreenCTM() 메서드는 SVG 요소에 적용된 현재 변환 행렬(CTM)을 반환, CTM은 SVG 요소의 현재 좌표 시스템과 사용자 에이전트의 초기 좌표 시스템 간의 관계를 정의
+      // inverse() 메서드는 해당 행렬의 역행렬을 반환
+      // matrixTransform() 메서드는 입력 행렬에 의해 변환된 새로운 point의 복사본을 반환
+      // transformedPoint는 현재 SVG 요소의 좌표 시스템에 올바르게 위치하게 됨.
+
+      // 마커 생성
+      const marker = (
+        <div
+          style={{
+            position: "absolute",
+            left: transformedPoint.x - 30,
+            top: transformedPoint.y - 31.25,
+          }}
+        >
+          <MarkerSvg
+            onClick={(event) => handleMarkerClick(event, targetStaion)}
+          />
+        </div>
+      );
+      setMarkerDiv(marker);
     }
   };
 
-  // 좌표에 해당하는 역 데이터 매칭
-  const getLocationValue = (x, y) => {
-    for (const station in stationCoordinates) {
-      const [stationX, stationY] = stationCoordinates[station];
-      if (
-        x >= stationX - 20 &&
-        x <= stationX + 20 &&
-        y >= stationY - 20 &&
-        y <= stationY + 20
-      ) {
-        console.log(`${station} 데이터 준비 완료`);
-        return {
-          station: station,
-          stationX: stationX,
-          stationY: stationY,
-        };
-      }
-    }
-  };
+  const handleMarkerClick = (event, targetStaion) => {
+    const clickX = event.clientX;
+    const clickY = event.clientY;
+    const targetBtn = document.elementFromPoint(clickX, clickY);
 
-  // 마커 이미지 그리는 함수
-  const drawMarker = (locationValue, x, y) => {
-    const markerCtx = markerCanvasRef.current.getContext("2d");
-    const markerImage = new Image();
-    markerImage.src = marker;
-    console.log("마커생성");
-
-    markerImage.onload = () => {
-      markerCtx.drawImage(
-        markerImage,
-        x,
-        y,
-        markerImage.width,
-        markerImage.height
-      );
-      setIsMarkerTrue(true);
-    };
-  };
-
-  // 마커 이미지를 지우는 함수
-  const clearMarker = () => {
-    const markerCanvas = markerCanvasRef.current;
-    const markerCtx = markerCanvas.getContext("2d");
-    markerCtx.clearRect(0, 0, markerCanvas.width, markerCanvas.height);
-    setIsMarkerTrue(false);
-    setLocationValue(false);
-    console.log("마커제거");
-  };
-
-  const markerClickEvent = (offsetX, offsetY) => {
-    console.log(locationValue);
-    if (
-      locationValue &&
-      offsetX >= locationValue.stationX - 58 &&
-      offsetX <= locationValue.stationX - 4 &&
-      offsetY >= locationValue.stationY - 56 &&
-      offsetY <= locationValue.stationY - 10
-    ) {
-      startResultClick(locationValue.station);
-      clearMarker();
-    } else if (
-      locationValue &&
-      offsetX <= locationValue.stationX + 58 &&
-      offsetX >= locationValue.stationX + 4 &&
-      offsetY >= locationValue.stationY - 56 &&
-      offsetY <= locationValue.stationY - 10
-    ) {
-      endResultClick(locationValue.station);
-      clearMarker();
-    } else {
-      clearMarker();
+    if (targetBtn.classList.contains("start")) {
+      startResultClick(eval(`"${targetStaion}"`));
+      setMarkerDiv(null);
+    } else if (targetBtn.classList.contains("end")) {
+      endResultClick(eval(`"${targetStaion}"`));
+      setMarkerDiv(null);
     }
   };
 
   return (
-    <div className="MetroMap">
-      <div
-        ref={mapRef}
-        onClick={handleMapClick}
-        onMouseDown={handleMouseDown}
-        onMouseUp={handleMouseUp}
-        onMouseMove={handleMouseMove}
-        onMouseLeave={handleMouseLeave}
-        onTouchStart={handleTouchStart}
-        onTouchEnd={handleTouchEnd}
-        onTouchMove={handleTouchMove}
-        // onWheel={handleWheel}
-        style={{
-          transform: `translate(${transform.x}px, ${transform.y}px) scale(${transform.scale})`,
-          transformOrigin: `0 0`,
-        }}
-      >
-        <canvas ref={mapCanvasRef}></canvas>
-        <canvas ref={markerCanvasRef}></canvas>
-      </div>
+    <div>
+      <TransformWrapper initialScale={1} minScale={0.9} maxScale={1.8}>
+        <TransformComponent>
+          <MapSvg onClick={handleMapClick} ref={svgRef} />
+          {markerDiv}
+        </TransformComponent>
+      </TransformWrapper>
     </div>
   );
 }
